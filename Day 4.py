f2caaf0b0b6eb8043b31ef71fadf8b7682ac0724
JenalAlpukat <bluttscreat@gmail.com>
2026-03-29 11:23:54 +0700

    lambda (1)

---
+++ Day 4.py
@@ -0,0 +1,6 @@
+kuadrat = lambda x: x*x
+angka = 5
+print(f"hasil kuadrat dari {angka} adalah {kuadrat(angka)}")
+#varibel = lambda parameter
+#variabel 2 = int
+#print (f"str {variabel 2}{variabel 1(variabel 2)}")
